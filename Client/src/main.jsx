dc749e97bad0bf8816d837c2b23049dcda3eac74
AnanthuManoj <120579729+AnanthuManoj@users.noreply.github.com>
2024-06-22 21:10:47 +0530

    make small correction to main.jsx file

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App.jsx';
 import ContextShare from './context/ContextShare.jsx';
 import './index.css';
@@ -11,9 +11,9 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ContextShare>
-      <BrowserRouter>
+      <HashRouter>
       <App />
-      </BrowserRouter>
+      </HashRouter>
     </ContextShare>
   </React.StrictMode>,
 )
