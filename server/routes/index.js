36b1e7f118262767c5bb73274d09dd0704c71190
shahil-mohammed-ms <115145954+shahil-mohammed-ms@users.noreply.github.com>
2024-07-18 13:13:10 +0530

    admin side coupon and review query problem resolved and server review routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,11 +7,8 @@ const categoryRoutes = require('./categoryRoutes');
 const addressRoutes = require('./addressRoutes');
 const bannerRoutes = require('./bannerRoutes');
 const cartRoutes = require('./cartRoutes')
-<<<<<<< HEAD:melonmagnets backend/routes/index.js
 const couponRoutes = require('./couponRoutes')
-=======
-const reviewRoutes = require('./reviewRoutes');
->>>>>>> d2581c4db32811b20310c94979836bd55a3ea374:server/routes/index.js
+const reviewRoutes = require('./reviewRoutes')
 const router = express.Router();
 
 router.use('/v1/auth', authRoutes);
@@ -22,10 +19,8 @@ router.use('/v1/orders', orderRoutes);
 router.use('/v1/address', addressRoutes);
 router.use('/v1/banner', bannerRoutes);
 router.use('/v1/cart', cartRoutes);
-<<<<<<< HEAD:melonmagnets backend/routes/index.js
 router.use('/v1/coupons', couponRoutes);
-
-=======
 router.use('/v1/reviews', reviewRoutes);
->>>>>>> d2581c4db32811b20310c94979836bd55a3ea374:server/routes/index.js
+
+
 module.exports = router;
