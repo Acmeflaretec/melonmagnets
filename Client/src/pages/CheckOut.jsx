04b8b638a2d5960dc7c2f8556b1eef3ac8fd0cbf
Abhijith Babu <abijithb2002@gmail.com>
2024-07-22 09:45:37 +0530

    prod:amount change for testing

--- Client/src/pages/CheckOut.jsx
+++ Client/src/pages/CheckOut.jsx
@@ -80,8 +80,8 @@ function CheckOut() {
   const handlePayment = () => {
     const options = {
       key: import.meta.env.VITE_APP_Razorpay_Api,
-      // amount: parseInt(subtotal < 299 ? subtotal + 79 : subtotal) * 100, 
-      amount: parseInt(subtotal - ((subtotal*discount)/100)  < 299 ? subtotal - ((subtotal*discount)/100)  + 79 : subtotal - ((subtotal*discount)/100) ) * 100, 
+      amount: 1 * 100, // price for testing purpose only
+      // amount: parseInt(subtotal - ((subtotal*discount)/100)  < 299 ? subtotal - ((subtotal*discount)/100)  + 79 : subtotal - ((subtotal*discount)/100) ) * 100, 
       currency: 'INR',
       name: 'MELON MAGNETS',
       description: 'Purchase course',
