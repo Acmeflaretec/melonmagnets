5528e2eb69875a2c8564f323ddce510ae3bc9acf
abdusamad8575 <samadpts786313@gmail.com>
2024-08-14 19:00:49 +0530

    payment failier cleare

--- Client/src/pages/CheckOut.jsx
+++ Client/src/pages/CheckOut.jsx
@@ -238,16 +238,15 @@ function CheckOut() {
 
   const handlePayment = () => {
     const options = {
-      key: 'rzp_test_wNhVz81BFxrIrL',
-      // key: import.meta.env.VITE_APP_Razorpay_Api,
-      // amount: parseInt(subtotal - discoutAmount < 299 ? subtotal - discoutAmount + 79 : subtotal - discoutAmount) * 100,
-      amount: 100,
+      key: import.meta.env.VITE_APP_Razorpay_Api,
+      amount: parseInt(subtotal - discoutAmount < 299 ? subtotal - discoutAmount + 79 : subtotal - discoutAmount) * 100,
+      // amount: 100,
       currency: 'INR',
       name: 'MELON MAGNETS',
       description: 'Purchase Magnets',  
       image: 'apple-touch-icon.png',         
       handler: function (response) {
-        handlePaymentSuccess(response);
+        handlePaymentSuccess();
       },
       modal: {
         ondismiss: function () {
@@ -263,15 +262,11 @@ function CheckOut() {
     const rzp = new window.Razorpay(options);
     rzp.open();
   };
-console.log('discountCode._id',discountCode._id);
 
-  const handlePaymentSuccess = async (response) => {
-    console.log('response12-',response);
+  const handlePaymentSuccess = async () => {
     
     try {
       const products = JSON.parse(localStorage.getItem('cartData')) || [];
-      console.log('sending intems',address, products, subtotal - discoutAmount < 299 ? subtotal - discoutAmount + 79 : subtotal - discoutAmount, userDetails?._id,discountCode._id);
-      
       const result = await addOrderApi({ ...address, products, amount: subtotal - discoutAmount < 299 ? subtotal - discoutAmount + 79 : subtotal - discoutAmount, userId: userDetails?._id, couponId: discountCode._id });
       console.log('results vannath',result);
       
